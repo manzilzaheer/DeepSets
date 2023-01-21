f7b25f25c2b50b19d0a0f3f0329d5c31a4fe4c24
Prateek Chanda <prateekkol21@gmail.com>
2023-01-21 17:14:17 +0530

    Fix options.py syntax DeepSets SetExpansion

--- SetExpansion/options.py
+++ SetExpansion/options.py
@@ -54,7 +54,7 @@ def readOptions():
                         help='true for GPU and false for CPU')
 
     try: parsed = vars(parser.parse_args());
-    except IOError, msg: parser.error(str(msg));
+    except IOError: msg: parser.error(str(msg));
 
     # Adjust model name to append current time
     time = strftime("-%d-%b-%Y-%X/", gmtime());
